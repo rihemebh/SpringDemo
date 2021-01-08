07e6c0fe2c7e0160d66befe62a9a5031dc0f72fe
rihem <rihemebenhassan@insat.u-carthage.tn>
2021-01-08 10:01:00 +0100

    repository

---
+++ Demo/src/main/java/com/example/demo/Costumer.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Costumer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- Demo/src/main/java/com/example/demo/RestController.java
+++ Demo/src/main/java/com/example/demo/RestController.java
@@ -10,28 +10,22 @@ public class RestController {
        we can also have another name and put a qualifier annotation @Qualifier(value="  ClassName")
 */
 
-    @Autowired
-    private Greeter greeter1;
+//    @Autowired
+//    private Greeter greeter;
+//
+//    @Autowired
+//    private FriendlyGreeter friendlyGreeter;
+//
+//
+//
+//    @RequestMapping("/hello")
+//    public String helloworld1(){ return greeter.greet(); }
+//
+//    @RequestMapping("/FriendlyGreeter")
+//    public String Friendly1(){ return friendlyGreeter.greet(); }
 
-    @Autowired
-    private FriendlyGreeter friendlyGreeter1;
+@Autowired
+    private CostumerRepository repo;
 
-    @Autowired
-    private Greeter greeter2;
-
-    @Autowired
-    private FriendlyGreeter friendlyGreeter2;
-
-    @RequestMapping("/hello1")
-    public String helloworld1(){ return greeter1.greet(); }
-
-    @RequestMapping("/FriendlyGreeter1")
-    public String Friendly1(){ return friendlyGreeter1.greet(); }
-
-    @RequestMapping("/hello2")
-    public String helloworld2(){ return greeter2.greet(); }
-
-    @RequestMapping("/FriendlyGreeter2")
-    public String Friendly2(){ return friendlyGreeter2.greet(); }
 
 }
